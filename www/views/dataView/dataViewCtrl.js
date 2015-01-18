770479448c9b95bba9d97f6e78ac768d5fa7547b
Randall <randall.krauskopf@gmail.com>
2015-01-18 11:59:07 -0600

    -latest changes for dynamic chart update.

--- www/views/dataView/dataViewCtrl.js
+++ www/views/dataView/dataViewCtrl.js
@@ -130,24 +130,6 @@
                         }
                     }
                 }
-
-                /*
-                for(var i = 0; i<allInput1.length; i++) {
-                    if (allInput1[i][0] >= $scope.fromToInitalDate && allInput1[i][0] <= $scope.fromToEndDate) {
-                        filteredInput1.push(allInput1[i]);
-                    }
-                }
-                for(var i = 0; i<allInput2.length; i++) {
-                    if (allInput2[i][0] >= $scope.fromToInitalDate && allInput2[i][0] <= $scope.fromToEndDate) {
-                        filteredInput2.push(allInput2[i]);
-                    }
-                }
-                for(var i = 0; i<allInput3.length; i++) {
-                    if(allInput3[i][0] >= $scope.fromToInitalDate && allInput3[i][0] <= $scope.fromToEndDate) {
-                        filteredInput3.push(allInput2[i]);
-                    }
-                }
-                */
             }
 
             setHighChartsData(allFilteredInputsObj);
@@ -171,7 +153,111 @@
                 options: {
                     chart: {
                         type: 'spline',
-                        zoomType: 'x'
+                        zoomType: 'x',
+                        events: {
+                            load: function() {
+                                setInterval(function () {
+
+                                    //get the most recent date to pass as a filter to the couch db instance
+                                    var lastIndex = allInputsObj['input 1'].length;
+
+                                    var latestDate = new Date(allInputsObj['input 1'][lastIndex-1][0]).toISOString();
+
+                                    //get Latest data
+                                    $http.get(couchInstanceURL + viewRoute + '?startkey="' + latestDate + "\"")
+                                        .success(function(data, status){
+                                            var extractedData = [];
+
+                                            //extract objects
+                                            for(var i = 0; i<data.rows.length; i++) {
+                                                extractedData.push(data.rows[i].value)
+                                            }
+
+                                            //sort the extractedData based on the dateTime
+                                            extractedData.sort(function(a,b){
+                                                // Turn your strings into dates, and then subtract them
+                                                // to get a value that is either negative, positive, or zero.
+                                                return new Date(a.dateTime) - new Date(b.dateTime);
+                                            });
+
+                                            //TODO: THe couchdb db will return a duplicate date, the one that was used to form the query in the first place. Need to remove it so
+                                            //duplicate data doesn't get added to the chart
+
+                                            //get the most recent date to pass as a filter to the couch db instance
+                                            var lastIndex = allInputsObj['input 1'].length;
+                                            var latestDate = new Date(allInputsObj['input 1'][lastIndex-1][0]);
+
+
+                                            var extractedLatestDate = new Date(extractedData[0]['dateTime']);
+
+                                            if(Number(latestDate) === Number(extractedLatestDate)) {
+                                                //remove the first element to prevent duplicate data when necessary
+                                                extractedData.splice(0, 1);
+                                            }
+
+                                            //get a list of the input names
+                                            var keyList = [];
+
+                                            //loop through each data row and grab all the unique keys that are available
+                                            //except the dateTime variable
+                                            for(var i = 0; i <extractedData.length; i++) {
+                                                var tempKeyList = keyList;
+
+                                                if(i === 0) {
+                                                    keyList = _.union(Object.keys(extractedData[i]), Object.keys(extractedData[i]));
+                                                }
+                                                else {
+                                                    keyList = _.union(tempKeyList, Object.keys(extractedData[i]));
+                                                }
+
+                                            }
+
+                                            //Remove the dateTime key from the keyList, that is x-axis data which
+                                            //does not need its own series input
+
+                                            for(var i = 0; i < keyList.length; i++) {
+                                                if(keyList[i] === 'dateTime') {
+                                                    keyList.splice(i,i);
+                                                }
+                                            }
+
+                                            //get the highchart series data
+                                            var chartSeries = $('#chart1').highcharts().series;
+
+                                            //Loop through and parse data into a dataset that ChartJS can consume.
+                                            for(var i = 0; i<extractedData.length; i++) {
+
+                                                for(var j = 0; j<keyList.length; j++) {
+                                                    //check to make sure key exists, this isn't a guarantee for each data entry
+                                                    var keyName = keyList[j];
+
+                                                    if(extractedData[i][keyName] !== undefined) {
+                                                        var date = new Date(extractedData[i].dateTime).toLocaleString();
+                                                        var tempArray = [date, Number(extractedData[i][keyName])];
+
+                                                        //Need to push into the master array and then dynamically add it to the chart
+                                                        allInputsObj[keyName].push(tempArray);
+
+                                                        //find the correct series by matching the input name to the series name
+                                                        chartSeries.forEach(function(series) {
+                                                            if(series.name === keyName) {
+                                                                series.addPoint(tempArray);
+                                                            }
+                                                        });
+                                                    }
+                                                }
+                                            }
+                                        })
+                                        .error(function(data, status) {
+
+                                        });
+
+                                    //var x = (new Date()).getTime(), // current time
+                                    //    y = Math.random();
+                                    //self.series[0].addPoint([x, y], true, true);
+                                }, 15000);
+                            }
+                        }
                     }
                 },
                 xAxis: {
@@ -189,5 +275,6 @@
                 loading: false
             }
         };
+
     });
 })();
